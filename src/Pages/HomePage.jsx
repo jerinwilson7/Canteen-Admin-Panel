5ace4c1eae80630a5f25f9c903eda4966c534617
jerinwilson7 <jerinwilson7@gmail.com>
2023-12-03 18:38:10 +0530

    added hompage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '../Components'
+
+function HomePage() {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default HomePage
