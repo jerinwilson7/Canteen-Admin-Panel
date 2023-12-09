2bca117b875050c4bbba84c0a6a32f947e5a3966
basil4693 <basilshaji4693@gmail.com>
2023-12-09 23:55:50 +0530

    added addproduct page

---
+++ src/Pages/AddProductPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddProductPage() {
+  return (
+    <div className='flex items-center justify-center'>
+        <h1>adddd</h1>
+    </div>
+  )
+}
+
+export default AddProductPage
