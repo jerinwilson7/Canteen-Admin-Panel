c9559bbd2d03d41d508d05327f5cf1c3199dd81a
basil4693 <basilshaji4693@gmail.com>
2023-12-15 00:07:29 +0530

    removed drop down

--- src/Components/SideBar/SideComponent.jsx
+++ src/Components/SideBar/SideComponent.jsx
@@ -1,19 +1,21 @@
-import React from "react";
 import SideBar from "./SideBar";
-import { ArrowLeftRight, BarChart3, MenuSquare, Utensils } from "lucide-react";
+import { ArrowLeftRight, BarChart3, MenuSquare,  Utensils } from "lucide-react";
 import Item from "./Item";
 import { NavLink } from "react-router-dom";
 
 function SideComponent() {
+ 
+
   return (
     <div>
       <SideBar>
         <NavLink exact to="/dash">
           <Item icon={<BarChart3 size={20} />} text="Dashboard" active />
         </NavLink>
-        <NavLink to="/food">
-          <Item icon={<Utensils size={20} />} text="Food" />
-        </NavLink>
+        <NavLink to='all-products'>
+            <Item icon={<Utensils size={20} />} text="Food" />
+          
+            </NavLink>
         <NavLink to="/menu">
           <Item icon={<MenuSquare size={20} />} text="Menu" />
         </NavLink>
