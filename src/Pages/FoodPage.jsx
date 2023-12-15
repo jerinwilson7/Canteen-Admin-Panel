2c1a6d1e21f3ca753f1828cb5e2cae7b08847f39
basil4693 <basilshaji4693@gmail.com>
2023-12-15 23:35:21 +0530

    updated food page

--- src/Pages/FoodPage.jsx
+++ src/Pages/FoodPage.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AllFood } from '../Components'
 
 function FoodPage() {
   
   const navigate = useNavigate()
 
   return (
-    <div className=''>
+    <div className='flex flex-col'>
       <div className=''>
-        <button className='border bg-paleOrange rounded-lg p-3'onClick={()=>navigate('/add-product')} >Add Food</button>
+        <button className='border bg-paleOrange rounded-lg p-3'onClick={()=>navigate('/add-food')} >Add Food</button>
       </div>
-    </div>
+      <div className='flex flex-row gap-4 w-full  m-2'>
+
+        <AllFood/>
+      </div>
+    </div> 
   )
 }
 
