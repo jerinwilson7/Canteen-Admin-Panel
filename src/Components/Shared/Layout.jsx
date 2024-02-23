20b69738b780f57a7da10c7705ca4042f1ec152a
jerinwilson7 <jerinwilson7@gmail.com>
2024-02-23 07:53:35 +0530

    header component completed

--- src/Components/Shared/Layout.jsx
+++ src/Components/Shared/Layout.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import SideBar from './SideBar'
+import Header from './Header'
 
 function Layout() {
     return (
         <div className='flex flex-row bg-neutral-100 w-screen h-screen overflow-hidden'>
-            <div className=''>
-                <SideBar />
+            <SideBar />
+            <div className='flex flex-col flex-1'>
+                <Header />
+                <div className='flex-1 p-4 min-h-0 overflow-auto'>
+                    {<Outlet />}
+                </div>
             </div>
-            <div>header</div>
-            <div>{<Outlet />}</div>
         </div>
     )
 }
--- src/Components/Shared/SideBar.jsx
+++ src/Components/Shared/SideBar.jsx
@@ -3,7 +3,7 @@ import { Sidebar_Links } from '../../constants'
 import { Link, useLocation } from 'react-router-dom'
 import classNames from 'classnames'
 
-const linkClass = 'flex items-center gap-2 font-light px-3 py-2 hover:bg-green-200 hover:no-underline active:bg-green-200  rounded-sm text-base'
+const linkClass = 'flex items-center gap-2 font-medium  py-2 px-3 my-1  cursor-pointer bg-opacity-50 transition duration-300 ease-in-out hover:text-seaGreen hover:bg-green-200 hover:no-underline active:bg-green-200  rounded-md text-medium'
 function SideBar() {
     return (
         <div className='flex flex-col bg-paleSilver p-4 w-60 border border-gray-200 h-screen'>
@@ -16,7 +16,24 @@ function SideBar() {
                     <SidebarLink key={item.key} item={item} />
                 ))}
             </div>
-            <div>admin</div>
+            {/* <div>admin</div> */}
+            <div className="mt-2 border-t flex p-3">
+                <img
+                    src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
+                    alt=""
+                    className="w-10 h-10 rounded-md"
+                />
+                <div
+                    className="
+              flex justify-between items-center
+              overflow-hidden transition-all w-52 ml-3"
+                >
+                    <div className="leading-4">
+                        <h4 className="font-semibold">John Doe</h4>
+                        <span className="text-xs text-gray-600">johndoe@gmail.com</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
@@ -32,7 +49,7 @@ function SidebarLink({ item }) {
         <Link
 
             to={item.path}
-            className={classNames(pathname === item.path ? ' from-indigo-200 to-indigo-100 text-seaGreen bg-green-200 transition duration-300 ease-in-out' : ' text-chineseBlack ', linkClass)}
+            className={classNames(pathname === item.path ? ' from-indigo-200 to-indigo-100 text-seaGreen bg-green-200 ' : ' text-chineseBlack ', linkClass)}
 
         >
             <span className='text-xl'>{item.icon}</span>
