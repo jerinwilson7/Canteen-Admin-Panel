2a193d13315635458e8bc1edd5bcb5f3bcfb1cc5
jerinwilson7 <jerinwilson7@gmail.com>
2024-02-26 00:52:17 +0530

    added title

--- src/Components/Shared/Header.jsx
+++ src/Components/Shared/Header.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { Menu, Popover, Transition } from '@headlessui/react'
-import { HiOutlineBell, HiOutlineSearch, HiOutlineChatAlt } from 'react-icons/hi'
+import { HiOutlineBell, HiOutlineChatAlt } from 'react-icons/hi'
 import { useNavigate } from 'react-router-dom'
 import classNames from 'classnames'
 
@@ -10,12 +10,7 @@ export default function Header() {
     return (
         <div className="bg-white h-16 px-4 flex items-center border-b border-gray-200 justify-between">
             <div className="relative">
-                <HiOutlineSearch fontSize={20} className="text-gray-400 absolute top-1/2 left-3 -translate-y-1/2" />
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    className="text-sm focus:outline-none active:outline-none border border-gray-300 w-[24rem] h-10 pl-11 pr-4 rounded-sm"
-                />
+                <h1 className='text-seaGreen font-bold font-Roboto '>BITE BUDDY</h1>
             </div>
             <div className="flex items-center gap-2 mr-2">
                 <Popover className="relative">
